c85f51bcaef0380f56c909a1d9a4ed7f4776eb5c
Anup Sukumaran <anup.sukumaran9@gmail.com>
2019-09-23 12:38:52 +0530

    - created the arc for the header view

--- StrechyHead/ViewController.swift
+++ StrechyHead/ViewController.swift
@@ -28,9 +28,11 @@ class ViewController: UIViewController {
         scrollView.parallaxHeader.height = 300
         scrollView.parallaxHeader.mode = .fill
         scrollView.parallaxHeader.minimumHeight = 88
-        print("Height = \(headerView.frame.height + 120)")
-        secondHeader.layer.cornerRadius = headerView.frame.height + 120
-        maxCurve = headerView.frame.height + 120
+        
+        print("Width = \(secondHeader.frame.width)")
+//        print("Height = \(headerView.frame.height + 120)")
+//        secondHeader.layer.cornerRadius = headerView.frame.height + 120
+//        maxCurve = headerView.frame.height + 120
 //        let circlePath = UIBezierPath(arcCenter: CGPoint(x: headerView.frame.midX, y: -200), radius: CGFloat(headerView.frame.width), startAngle: CGFloat(0), endAngle: CGFloat(Double.pi * 2), clockwise: true)
 //
 //
@@ -68,54 +70,54 @@ extension ViewController: UIScrollViewDelegate {
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
 //        let offsetY = CGFloat(max(-(scrollView.contentOffset.y + scrollView.contentInset.top), 0.0))
 //        self.progress = min(max(offsetY / frame.size.height, 0.0), 1.0)
-         print("headerView.frame.height = \(headerView.frame.height)")
-        let offset = scrollView.contentOffset.y
-       // let offset = headerView.frame.height - (y + headerView.frame.height)
-  //      let totalScroll: CGFloat = scrollView.contentSize.height - scrollView.bounds.size.height
-        
-       // let tt = offset / totalScroll
-    //    print("totalScroll = \(totalScroll)")
-       
-        
-      //  let offset = scrollView.contentOffset.y
-        print("offset = \(offset)")
-        if scrollMovingUp {
-            //decrease the curve
-//            let newVal =  min(max(offset, 0), maxCurve)
-//            print("newValUp  \(newVal)")
-           // if tt > 0 {
-               // print("vall = \(maxCurve / tt)")
-            let valUp = max(headerView.frame.height, 0)
-            print("valUp = \(valUp)")
-            secondHeader.layer.cornerRadius =  valUp
-           // secondHeader.layer.cornerRadius = secondHeader.layer.cornerRadius - (maxCurve / totalScroll)
-//            UIView.animate(withDuration: 0.5) {
-//                self.view.layoutIfNeeded()
-//            }
-           // }
-           
-        } else {
-           // increase the curve
-           // let newVal = min(0, abs(offset) )
-          //  print("newValDown  \(newVal)")
-           // secondHeader.layer.cornerRadius = newVal
-            
-            let valDown = min(400, headerView.frame.height)
-            print("valDown = \(valDown)")
-            secondHeader.layer.cornerRadius =  valDown + (400 - valDown)
-//            if headerView.frame.height < 400 {
+//         print("headerView.frame.height = \(headerView.frame.height)")
+//        let offset = scrollView.contentOffset.y
+//       // let offset = headerView.frame.height - (y + headerView.frame.height)
+//  //      let totalScroll: CGFloat = scrollView.contentSize.height - scrollView.bounds.size.height
 //
-//                secondHeader.layer.cornerRadius =  min(400, headerView.frame.height)
-//            } else {
-//                 secondHeader.layer.cornerRadius = 400
-//            }
-            
-//            UIView.animate(withDuration: 0.5) {
-//                self.view.layoutIfNeeded()
-//            }
-            
-        }
-        
+//       // let tt = offset / totalScroll
+//    //    print("totalScroll = \(totalScroll)")
+//
+//
+//      //  let offset = scrollView.contentOffset.y
+//        print("offset = \(offset)")
+//        if scrollMovingUp {
+//            //decrease the curve
+////            let newVal =  min(max(offset, 0), maxCurve)
+////            print("newValUp  \(newVal)")
+//           // if tt > 0 {
+//               // print("vall = \(maxCurve / tt)")
+//            let valUp = max(headerView.frame.height, 0)
+//            print("valUp = \(valUp)")
+//            secondHeader.layer.cornerRadius =  valUp
+//           // secondHeader.layer.cornerRadius = secondHeader.layer.cornerRadius - (maxCurve / totalScroll)
+////            UIView.animate(withDuration: 0.5) {
+////                self.view.layoutIfNeeded()
+////            }
+//           // }
+//
+//        } else {
+//           // increase the curve
+//           // let newVal = min(0, abs(offset) )
+//          //  print("newValDown  \(newVal)")
+//           // secondHeader.layer.cornerRadius = newVal
+//
+//            let valDown = min(400, headerView.frame.height)
+//            print("valDown = \(valDown)")
+//            secondHeader.layer.cornerRadius =  valDown + (400 - valDown)
+////            if headerView.frame.height < 400 {
+////
+////                secondHeader.layer.cornerRadius =  min(400, headerView.frame.height)
+////            } else {
+////                 secondHeader.layer.cornerRadius = 400
+////            }
+//
+////            UIView.animate(withDuration: 0.5) {
+////                self.view.layoutIfNeeded()
+////            }
+//
+//        }
+//
     //    secondHeader.layer.cornerRadius = 0
 //
 //        let offset = scrollView.contentOffset.y
